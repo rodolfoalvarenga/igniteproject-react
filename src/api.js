ebc7ee6303f9584b08b9802fd9f733600f1ce9dc
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-07 08:32:09 -0300

    10. Fetching Game Detail

--- src/api.js
+++ src/api.js
@@ -37,3 +37,10 @@ const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+// Game details
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+
+// Game screenshots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots`;
