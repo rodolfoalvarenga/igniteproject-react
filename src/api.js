956abb4dd1d74c946b8ced29fa1d3624bdb5821f
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-07 15:14:17 -0300

    20. Nav Searching

--- src/api.js
+++ src/api.js
@@ -44,3 +44,7 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 // Game screenshots
 export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
+
+// Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page-size=9`;
--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 //Media resize
 export const smallImage = (imagePath, size) => {
+  if (!imagePath) return null;
   const image = imagePath.match(/media\/screenshots/)
     ? imagePath.replace(
         "media/screenshots",
